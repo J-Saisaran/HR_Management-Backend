3fcefcd36d9fa41cb6cb3db6c56c68cbabeb423a
J-Saisaran <saransai027@gmail.com>
2024-08-25 00:12:20 +0530

    Hr logout update

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 
-const AttendanceSchema = new mongoose.Schema({
-    employee: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Employee', 
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['Present', 'Absent',  'Leave','Late'], 
-        required: true,
-    },
-    notes: {
-        type: String,
-        trim: true,
-    },
-}, { timestamps: true });
+    const AttendanceSchema = new mongoose.Schema({
+        employee: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Employee', 
+            required: true,
+        },
+        date: {
+            type: Date,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ['Present', 'Absent',  'Leave','Late'], 
+            required: true,
+        },
+        notes: {
+            type: String,
+            trim: true,
+        },
+    }, { timestamps: true });
 
-module.exports = mongoose.model('Attendance', AttendanceSchema);
+    module.exports = mongoose.model('Attendance', AttendanceSchema);
