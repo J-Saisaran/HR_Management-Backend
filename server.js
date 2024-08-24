42a953352ec07b6a339146eaaca5283224e37a89
J-Saisaran <saransai027@gmail.com>
2024-08-25 00:46:02 +0530

    cachae updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-require("./dbconnection/connectdb")
+require("./dbconnection/connectdb"); // Ensure this path is correct
 const PORT = process.env.PORT || 5000;
 
 
